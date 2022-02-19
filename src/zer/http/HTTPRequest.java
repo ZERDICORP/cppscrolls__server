55dae9fa65513d628996b94f8f457c830f8aa82b
ZERDICORP <nikolaianikin2002@gmail.com>
2022-02-19 22:58:42 +0300

    added path parsing

--- src/zer/http/HTTPRequest.java
+++ src/zer/http/HTTPRequest.java
@@ -8,25 +8,29 @@ import java.util.Arrays;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
+import constants.Server;
+
 
 
 public class HTTPRequest
 {
 	private HashMap<String, String> fields = new HashMap<>();
+	private ArrayList<String> path;
 	private byte[] body = new byte[0];
 
 	{
 		fields.put("Type", "GET");
 		fields.put("Path", "/");
 	}
-
-	public String get(String key) { return this.fields.get(key); }
-	public HTTPRequest set(String key, String sValue) { this.fields.put(key, sValue); return this; }
 	
-	public void setBody(String body) { this.body = body.getBytes(); }
-	public void setBody(byte[] body) { this.body = body; }
+	public String path(int index) { return path.get(index + Server.API_PREFIX.split("/").length); }
+	public String get(String key) { return fields.get(key); }
+	public HTTPRequest set(String key, String sValue) { fields.put(key, sValue); return this; }
 	
-	public boolean bodyIsEmpty() { return this.body.length == 0; }
+	public void setBody(String b) { body = b.getBytes(); }
+	public void setBody(byte[] b) { body = b; }
+	
+	public boolean bodyIsEmpty() { return body.length == 0; }
 	public boolean parse(String request) throws Exception
 	{
 		String[] parts = request.split("\r\n\r\n");
@@ -41,17 +45,19 @@ public class HTTPRequest
 		if (statusLine.length != 3)
 			return false;
 
-		this.fields.put("Type", statusLine[0]);
-		this.fields.put("Path", statusLine[1]);
+		fields.put("Type", statusLine[0]);
+		fields.put("Path", statusLine[1]);
 
 		for (String line : lines)
 		{
 			String[] temp = line.split(": ");
-			this.fields.put(temp[0], temp[1]);
+			fields.put(temp[0], temp[1]);
 		}
 
 		if (parts.length > 1)
 			fields.put("Body", parts[1]);
+
+		path = new ArrayList<>(Arrays.asList(fields.get("Path").split("/")));
 
 		return true;
 	}
@@ -59,17 +65,17 @@ public class HTTPRequest
 	public byte[] make() throws IOException
 	{
 		String requestString = new String();
-		requestString += this.fields.get("Type") + " " + this.fields.get("Path") + " HTTP/1.1\r\n";
-		if (this.body.length != 0)
+		requestString += fields.get("Type") + " " + fields.get("Path") + " HTTP/1.1\r\n";
+		if (body.length != 0)
 		{
-			requestString += "Connection-Type: " + this.fields.get("Content-Type") + "\r\n";
-			requestString += "Content-Length: " + this.body.length;
+			requestString += "Connection-Type: " + fields.get("Content-Type") + "\r\n";
+			requestString += "Content-Length: " + body.length;
 		}
 		requestString += "\r\n\r\n";
 
 		ByteArrayOutputStream requestStream = new ByteArrayOutputStream();
 		requestStream.write(requestString.getBytes());
-		requestStream.write(this.body);
+		requestStream.write(body);
 
 		return requestStream.toByteArray();
 	}
