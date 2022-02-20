f97ef94f12552c953561b532574a8401d3c51a57
ZERDICORP <nikolaianikin2002@gmail.com>
2022-02-20 17:06:12 +0300

    added new constants

--- src/constants/Server.java
+++ src/constants/Server.java
@@ -13,6 +13,10 @@ public interface Server
   String EMAIL_SENDER = "nikolaianikin2002@gmail.com";
   String EMAIL_SENDER_PASSWORD = "7|C]0JMyC,-`a^1oE$>w";
 
+	String DATABASE_USER = "root";
   String DATABASE_NAME = "cppscrolls";
   String DATABASE_PASSWORD = "zerdicorp1937";
+
+	String SQL_DRIVER = "org.mariadb.jdbc.Driver";
+	String SQL_CONNECTION_STRING = "jdbc:mariadb://localhost:3306/" + Server.DATABASE_NAME + "?autoReconnect=true";
 }
